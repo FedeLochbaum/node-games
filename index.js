cc89d0b7ea279536dcedd94a596a53971bfb3eea
Federico Lochbaum <fede@SCVsofts-MacBook-Pro-2.local>
2019-05-09 17:42:55 -0300

    define an interface for select a languague

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
 
-var game = process.argv[2];
+const US = 'us'
+const ES = 'es'
+const DEFAULT_LANG = US
+const LANGUAGUES = { ES, US } 
+
+var game = process.argv[2]
+var lang = process.argv[3]
+
+const LANG = LANGUAGUES[LANGUAGUES[lang] ? lang : DEFAULT_LANG]
 
 if (!game) {
-  console.log('usage: node-games <game>');
-  console.log('');
-  console.log('Games');
+  console.log('usage: node-games <game> <languague>\n');
+  console.log('Games:');
   console.log('- spacecraft');
   console.log('- snake');
-  console.log('- tanks');
+  console.log('- tanks\n');
+  console.log('Optional params: \n')
+  console.log('Languagues: ')
+  console.log(Object.keys(LANGUAGUES).map( key => `- ${LANGUAGUES[key]}`).join('\n'))
+  console.log(`The default languague is ${DEFAULT_LANG}\n`)
   return;
 }
+console.log(`Running the game in ${LANG}`)
 
 require('babel-polyfill');
 
